234d59124ffe632eb25e9e1ca2e7879bcdfb62c6
Hector Rodriguez <hector@hrod.dev>
2023-02-10 18:56:38 -0500

    Add cors setting

--- src/main/java/com/hroddev/todoapi/DemoProjectOfToDoApiApplication.java
+++ src/main/java/com/hroddev/todoapi/DemoProjectOfToDoApiApplication.java
@@ -2,11 +2,13 @@ package com.hroddev.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class DemoProjectOfToDoApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoProjectOfToDoApiApplication.class, args);
 	}
 
